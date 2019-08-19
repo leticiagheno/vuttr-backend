0715a7f6ca563e2d1274cc98bffdfb35d6631893
Letícia <leeti@live.com>
2019-08-18 21:09:42 -0300

    :recycle: Update app with db connection

--- dist/app.js
+++ dist/app.js
@@ -2,10 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const route_1 = require("./routes/route");
+const applicationContext_1 = require("./infrastructure/applicationContext");
 class App {
     constructor() {
-        this.routes = new route_1.Route();
         this.app = express();
+        this.routes = new route_1.Route();
+        this.context = new applicationContext_1.default();
+        this.context.createConnection();
         this.routes.routes(this.app);
     }
 }
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import * as express from "express";
 import { Route } from "./routes/route";
+import ApplicationContext from "./infrastructure/applicationContext";
 
 class App {
-  public app: express.Application;
-  public routes: Route = new Route();
+    public app: express.Application;
+    public routes: Route;
+    private context: ApplicationContext;
 
   constructor() {
       this.app = express();
+      this.routes = new Route();
+      this.context = new ApplicationContext();
+      this.context.createConnection();
       this.routes.routes(this.app);
   }
 }
