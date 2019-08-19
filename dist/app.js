43ea1b691c6dddc10d31d717e5d1358dfa49bded
Letícia <leeti@live.com>
2019-08-18 21:14:56 -0300

    :recycle: Update app class adding body-parser

--- dist/app.js
+++ dist/app.js
@@ -3,13 +3,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const route_1 = require("./routes/route");
 const applicationContext_1 = require("./infrastructure/applicationContext");
+const BodyParser = require("body-parser");
 class App {
     constructor() {
         this.app = express();
         this.routes = new route_1.Route();
         this.context = new applicationContext_1.default();
         this.context.createConnection();
+        this.middler();
         this.routes.routes(this.app);
+    }
+    middler() {
+        this.app.use(BodyParser.json());
+        this.app.use(this.bodyParser.urlencoded({ extend: false }));
     }
 }
 exports.default = new App();
--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,28 @@
 import * as express from "express";
 import { Route } from "./routes/route";
 import ApplicationContext from "./infrastructure/applicationContext";
+import * as BodyParser from "body-parser";
 
 class App {
     public app: express.Application;
     public routes: Route;
     private context: ApplicationContext;
+    private bodyParser;
 
   constructor() {
       this.app = express();
       this.routes = new Route();
       this.context = new ApplicationContext();
       this.context.createConnection();
+      this.middler();
       this.routes.routes(this.app);
   }
+
+  middler() {
+    this.app.use(BodyParser.json());
+    this.app.use(this.bodyParser.urlencoded({extend: false}));
+  }
+
 }
 
 export default new App();
